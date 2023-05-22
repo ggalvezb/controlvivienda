825b23159f14ea9dce90b13a6d751ad6cba2647a
Gonzalo Galvez <ggalvezb@live.com>
2023-05-22 15:39:07 -0400

    Actualizacion CORS

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 #from fastapi.staticfiles import StaticFiles
 from routers import supervisores,psat,administradores,proyectos,auth_user
+from fastapi.middleware.cors import CORSMiddleware
 
 # Inicia el servidor: uvicorn main:app --reload
 
 app=FastAPI()
+
 
 #routers
 app.include_router(supervisores.router)
@@ -15,6 +17,17 @@ app.include_router(auth_user.router)
 
 #app.mount("/static", StaticFiles(directory="static"),name="static")
 
+# Configurar los orígenes permitidos como '*'
+origins = ["*"]
+
+# Agregar el middleware CORS a la aplicación
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return{"message":"Hello World"}
